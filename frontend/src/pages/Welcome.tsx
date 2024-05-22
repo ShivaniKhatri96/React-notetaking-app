3d305d0352f56939bdc5f8d1af0237d2b4242453
Shivani Khatri <khatri.shivani96@gmail.com>
2024-05-22 18:26:42 +0200

    using MUI5 in welcome page instead of vanilla CSS

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,19 +1,44 @@
 import "./Welcome.css";
 import PostIt from "../assets/notes.png";
 import Logo from "../assets/logo.png";
-
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 const Welcome = () => {
   return (
     <main className="welcome-box">
       <img alt="post-it picture" className="notes-image" src={PostIt} />
-      <div className="Noteworthy-box">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+          alignItems: { xs: "center", lg: "start" },
+        }}
+      >
         <img alt="logo" className="logo-icon" src={Logo} />
-        <h1 className="welcome-title">
+        <Typography
+          variant="body1"
+          sx={{
+            fontWeight: 500,
+            textAlign: { xs: "center", lg: "left" },
+          }}
+        >
           Ready to embark on a note-taking journey? <br /> Log in now to get
           started!
-        </h1>
-        <button className="log-in-button">Log in</button>
-      </div>
+        </Typography>
+        <Button
+          variant="outlined"
+          color="inherit"
+          sx={{
+            width: "8rem",
+            border: "solid 2px #000",
+            borderRadius: "8px",
+          }}
+        >
+          Log in
+        </Button>
+      </Box>
     </main>
   );
 };
