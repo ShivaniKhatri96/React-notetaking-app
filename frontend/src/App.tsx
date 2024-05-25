703f8fe3942ab93cf8bd279804327f023ed38f2c
Shivani Khatri <khatri.shivani96@gmail.com>
2024-05-25 19:45:19 +0200

    logging in based on authentication token

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,35 @@
 import "./App.css";
 import Welcome from "./pages/Welcome";
 import Navbar from "./components/header/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import MyNotes from "./pages/MyNotes";
 import Home from "./pages/Home";
+import { useAppSelector } from "./app/hooks";
 
 function App() {
+  const token = useAppSelector((state) => state.auth.token);
   return (
     <div className="app">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/welcome" element={<Welcome />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/my-notes" element={<MyNotes />} />
+          {token === null ? (
+            <>
+              <Route path="/welcome" element={<Welcome />} />
+              <Route path="*" element={<Navigate replace to="/welcome" />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/my-notes" element={<MyNotes />} />
+              <Route path="*" element={<Navigate replace to="/" />} />
+            </>
+          )}
         </Routes>
       </Router>
     </div>
