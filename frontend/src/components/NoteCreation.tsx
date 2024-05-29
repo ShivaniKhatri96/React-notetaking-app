f105e15d3622e3032752fda93f9d5b5a1e090bd8
Shivani Khatri <khatri.shivani96@gmail.com>
2024-05-29 22:15:36 +0200

    buttons added to note creation

--- frontend/src/components/NoteCreation.tsx
+++ frontend/src/components/NoteCreation.tsx
@@ -1,34 +1,42 @@
-import { Paper, TextField } from "@mui/material";
+import { Button, Paper, TextField } from "@mui/material";
 import { useState } from "react";
 
 const NoteCreation = () => {
   const [isCreatingNote, setIsCreatingNote] = useState<boolean>(false);
 
   return (
-    <div>
-      <Paper>
-        {isCreatingNote && (
-          <TextField
-            id="outlined-basic"
-            placeholder="Title"
-            fullWidth
-            size="small"
-            sx={{ "& fieldset": { border: "none" } }}
-          />
-        )}
-
+    <Paper>
+      {isCreatingNote && (
         <TextField
           id="outlined-basic"
-          placeholder="Take a note..."
-          multiline
+          placeholder="Title"
           fullWidth
-          rows={isCreatingNote ? 4 : 1}
           size="small"
-          onClick={() => setIsCreatingNote(true)}
           sx={{ "& fieldset": { border: "none" } }}
         />
-      </Paper>
-    </div>
+      )}
+
+      <TextField
+        id="outlined-basic"
+        placeholder="Take a note..."
+        multiline
+        fullWidth
+        rows={isCreatingNote ? 4 : 1}
+        size="small"
+        onClick={() => setIsCreatingNote(true)}
+        sx={{ "& fieldset": { border: "none" } }}
+      />
+      {isCreatingNote && (
+        <>
+          <Button variant="outlined" color="inherit" size="small">
+            Cancel
+          </Button>
+          <Button variant="contained" color="success" size="small">
+            Create
+          </Button>
+        </>
+      )}
+    </Paper>
   );
 };
 
