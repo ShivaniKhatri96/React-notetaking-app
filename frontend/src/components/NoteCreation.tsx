60decd96f37350fcf6af4e9f8f855b51a0bc4928
Shivani Khatri <khatri.shivani96@gmail.com>
2024-05-29 23:09:02 +0200

    noteCreation box closes when clicked outside it now

--- frontend/src/components/NoteCreation.tsx
+++ frontend/src/components/NoteCreation.tsx
@@ -1,11 +1,40 @@
-import { Button, Paper, TextField } from "@mui/material";
-import { useState } from "react";
+import { Box, Button, Paper, TextField } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 
 const NoteCreation = () => {
   const [isCreatingNote, setIsCreatingNote] = useState<boolean>(false);
+  const paperRef: any = useRef(null);
+
+  const handleClickOutside = (e: any) => {
+    if (
+      isCreatingNote &&
+      paperRef.current &&
+      !paperRef.current.contains(e.target)
+    ) {
+      setIsCreatingNote(false);
+      //empty the textfield as well..
+    }
+  };
+  useEffect(() => {
+    if (isCreatingNote) {
+      document.addEventListener("mousedown", handleClickOutside);
+      // Clean up function before running new effect
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }
+  }, [isCreatingNote]);
 
   return (
-    <Paper>
+    <Paper
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignSelf: "center",
+        width: { xs: "100%", lg: "40rem" },
+      }}
+      ref={paperRef}
+    >
       {isCreatingNote && (
         <TextField
           id="outlined-basic"
@@ -27,14 +56,21 @@ const NoteCreation = () => {
         sx={{ "& fieldset": { border: "none" } }}
       />
       {isCreatingNote && (
-        <>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "end",
+            gap: "0.5rem",
+            padding: "8.5px 14px",
+          }}
+        >
           <Button variant="outlined" color="inherit" size="small">
             Cancel
           </Button>
           <Button variant="contained" color="success" size="small">
             Create
           </Button>
-        </>
+        </Box>
       )}
     </Paper>
   );
