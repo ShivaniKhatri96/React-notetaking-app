e3ba99f92ff3ea37a131bad56b5b7a545533f3ae
Shivani Khatri <khatri.shivani96@gmail.com>
2024-06-02 23:56:36 +0200

    card header updated based on user authentication.

--- frontend/src/components/notes/NoteCard.tsx
+++ frontend/src/components/notes/NoteCard.tsx
@@ -6,6 +6,10 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Person from "../../assets/person.png";
+import { useAppSelector } from "../../app/hooks";
+import { purple } from "@mui/material/colors";
+import LockIcon from "@mui/icons-material/Lock";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 
 interface NoteObj {
   content: string;
@@ -19,8 +23,11 @@ interface NoteProps {
   note: NoteObj;
 }
 const NoteCard = ({ note }: NoteProps) => {
+  // only loggedInUser should be able to edit, delete or make a note private
+  const authUser = useAppSelector((state) => state.auth.user);
+  const isAuthUser = authUser?.userId === note.user;
   return (
-    <Card sx={{ height: 350 }}>
+    <Card sx={{ height: 350, color: "#333333" }}>
       <CardHeader
         avatar={
           <Avatar
@@ -34,14 +41,36 @@ const NoteCard = ({ note }: NoteProps) => {
           />
         }
         action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
+          isAuthUser && (
+            <IconButton aria-label="settings">
+              <MoreVertIcon sx={{ color: "#fff" }} />
+            </IconButton>
+          )
         }
-        title={note.noteCreator}
-        subheader="public"
+        title={
+          <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+            {note.noteCreator}
+          </Typography>
+        }
+        subheader={
+          <Typography
+            variant="caption"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            {note.privacy ? (
+              <LockIcon fontSize="inherit" />
+            ) : (
+              <PeopleAltIcon fontSize="inherit" />
+            )}
+            . {note.privacy ? "Private" : "Public"} Note
+          </Typography>
+        }
         sx={{
-          bgcolor: "#f6f4f4",
+          bgcolor: isAuthUser ? purple[200] : "#f3f0f0",
+          color: isAuthUser && "#ffffff",
           boxShadow:
             "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px",
         }}
