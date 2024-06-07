19bf3628032e0a5ef397b98688b9d02d3861bbfd
Shivani Khatri <khatri.shivani96@gmail.com>
2024-06-07 21:27:43 +0200

    base url added for local and production environment

--- frontend/src/axios/instance.ts
+++ frontend/src/axios/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiInstance = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: `${import.meta.env.VITE_BASE_URI}/api/`,
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
