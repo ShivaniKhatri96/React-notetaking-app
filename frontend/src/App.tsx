01850baa24d8fbd5e21064577660b16ba8930655
Shivani Khatri <khatri.shivani96@gmail.com>
2024-06-10 19:10:20 +0200

    vercel.json file added to avoid a bug that shows 404 page when refreshing in deployed app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -38,7 +38,7 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="/*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
