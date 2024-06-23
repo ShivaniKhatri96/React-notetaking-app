5b19838bd23efeab4fd7b8dfb254630af030a244
Shivani Khatri <khatri.shivani96@gmail.com>
2024-06-23 23:18:01 +0200

    log in with cypress successful

--- frontend/cypress/e2e/log-in.cy.ts
+++ frontend/cypress/e2e/log-in.cy.ts
@@ -1,5 +1,9 @@
 describe('template spec', () => {
     it('passes', () => {
         cy.visit('http://127.0.0.1:5173/')
+        cy.contains('button','Log in').click()
+        cy.get('input[type="username"]').type("Ron")
+        cy.get('input[type="password"]').type("Ron1")
+        cy.get('button[type="submit"]').click()
     })
 })
